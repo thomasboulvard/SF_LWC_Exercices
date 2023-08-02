6c503ad1ef785a37aa64b974ffcf78e3e7068d2d
Thomas Boulvard <thomas.boulvard@gmail.com>
2023-08-02 17:33:20 +0200

    Modify LWC02, LWC03 & LWC04 to retrieve the Categories and the Links. Everything is working fine except the CSS

--- force-app/main/default/lwc/lWC_03_CustomLinksCategory/lWC_03_CustomLinksCategory.js
+++ force-app/main/default/lwc/lWC_03_CustomLinksCategory/lWC_03_CustomLinksCategory.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getCategoriesList from '@salesforce/apex/AP01_DAO_CustomLinks.getCategoriesList';
 
-export default class LWC_03_CustomLinksCategory extends LightningElement {}
+export default class LWC_03_CustomLinksCategory extends LightningElement {
+    @wire(getCategoriesList)
+    categories;
+}
--- force-app/main/default/lwc/lWC_04_CustomLinksURL/lWC_04_CustomLinksURL.js
+++ force-app/main/default/lwc/lWC_04_CustomLinksURL/lWC_04_CustomLinksURL.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getLinksList from '@salesforce/apex/AP01_DAO_CustomLinks.getLinksList';
 
-export default class LWC_04_CustomLinksURL extends LightningElement {}
+export default class LWC_04_CustomLinksURL extends LightningElement {
+    @wire(getLinksList)
+    links;
+}
